147fc71fd13926bb82a87cde1486a4210eb18547
Kacper <kmlynkowiak@edu.cdv.pl>
2024-10-04 16:57:39 +0200

    changes to make it more compatible with 2.7 version

--- upload_s3.py
+++ upload_s3.py
@@ -1,6 +1,7 @@
 # This script collects EC2 instance metadata and uploads it to an S3 bucket
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function  # dla zgodności z Pythonem 2.7
 
 import boto3
 from botocore.exceptions import ClientError
@@ -28,12 +29,12 @@ try:
         for param, endpoint in ec2_params.items():
             try:
                 # Fetch metadata from the EC2 instance
-                response = requests.get(f"{meta_data_url}/{endpoint}")
+                response = requests.get("{}/{}".format(meta_data_url, endpoint))
                 response.raise_for_status()  # Catch any HTTP errors
-                fh.write(f"{param}: {response.text}\n")  # Write metadata to file
+                fh.write("{}: {}\n".format(param, response.text))  # Write metadata to file
             except requests.RequestException as e:
                 # Print error message if metadata retrieval fails
-                print(f"Failed to retrieve {param}: {str(e)}")
+                print("Failed to retrieve {}: {}".format(param, str(e)))
         
         # Fetch operating system-related information
         os_name = os.popen("grep '^NAME' /etc/os-release | cut -d'=' -f2").read().strip()
@@ -42,13 +43,13 @@ try:
         users = os.popen("grep -E 'bash|sh' /etc/passwd | awk -F ':' '{print $1}' | xargs echo").read().strip()
 
         # Write OS name, version, and users to the file
-        fh.write(f"OS NAME: {os_name}\n")
-        fh.write(f"OS VERSION: {os_version}\n")
-        fh.write(f"Loginable Users: {users}\n")
+        fh.write("OS NAME: {}\n".format(os_name))
+        fh.write("OS VERSION: {}\n".format(os_version))
+        fh.write("Loginable Users: {}\n".format(users))
 
 # Catch and handle request errors
 except requests.RequestException as e:
-    print(f"Failed to retrieve EC2 metadata: {str(e)}")
+    print("Failed to retrieve EC2 metadata: {}".format(str(e)))
 
 # Upload the metadata file to the specified S3 bucket
 s3_bucket_name = 'applicant-task/r5d4'  # Updated to the correct bucket name
@@ -58,16 +59,16 @@ s3 = boto3.client('s3')  # Initialize the S3 client
 try:
     with open(ec2InsDatafile, 'r') as fh:
         # Fetch the EC2 instance ID to use in the S3 object key
-        instance_id = requests.get(f"{meta_data_url}/instance-id").text.strip()
+        instance_id = requests.get("{}/instance-id".format(meta_data_url)).text.strip()
         # Upload the file to S3 with the instance ID as part of the file name
         s3.put_object(
             Bucket=s3_bucket_name,
-            Key=f'system_info_{instance_id}.txt',
+            Key='system_info_{}.txt'.format(instance_id),
             Body=fh.read()
         )
     # Print success message upon successful upload
-    print(f"File has been uploaded into {s3_bucket_name} S3 bucket with instance_id key.")
+    print("File has been uploaded into {} S3 bucket with instance_id key.".format(s3_bucket_name))
     
 # Catch and handle errors during file upload or S3 operations
 except (ClientError, IOError) as e:
-    print(f"Failed to upload file to S3: {str(e)}")
+    print("Failed to upload file to S3: {}".format(str(e)))
